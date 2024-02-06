84163d6a3350da332480fdb638e5227b0e949ab8
Anton Fredriksson <79942428+Phoeenix05@users.noreply.github.com>
2024-02-06 12:57:08 +0200

    initial commit: copied template

---
+++ src/main/java/github/phoeenix05/simplebackpacks/SimpleBackpacksDataGenerator.java
@@ -0,0 +1,11 @@
+package github.phoeenix05.simplebackpacks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleBackpacksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
